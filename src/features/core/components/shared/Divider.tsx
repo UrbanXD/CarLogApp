1a0677b1b4c69d468de1ad69ff82bfad94729dc5
UrbanXD <uadamvidi@gmail.com>
2024-12-20 22:42:03 +0100

    Divider added for replace divider from rn paper

---
+++ src/features/core/components/shared/Divider.tsx
@@ -0,0 +1,46 @@
+import { View, StyleSheet, ColorValue } from "react-native";
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen";
+import { theme } from "../../constants/theme";
+
+interface DividerProps {
+    size?: number
+    thickness?: number
+    color?: ColorValue | string
+    margin?: number
+    isVertical?: boolean
+}
+
+const Divider: React.FC<DividerProps> = ({
+    isVertical = false,
+    size = isVertical ? hp(100) : wp(100),
+    thickness = 1,
+    color = theme.colors.white,
+    margin = 0,
+}) => {
+    const styles = useStyles(size, thickness, color, margin);
+
+    return (
+        <View style={ isVertical ? styles.verticalLine : styles.horizontalLine } />
+    )
+}
+
+const useStyles = (size: number, thickness: number, color: ColorValue | string, margin: number) => {
+    return StyleSheet.create({
+        horizontalLine: {
+            alignSelf: "center",
+            width: size,
+            height: thickness,
+            marginVertical: margin,
+            backgroundColor: color,
+        },
+        verticalLine: {
+            alignSelf: "center",
+            width: thickness,
+            height: size,
+            marginHorizontal: margin,
+            backgroundColor: color,
+        }
+    })
+}
+
+export default Divider;
