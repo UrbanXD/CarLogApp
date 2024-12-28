6b6c4789cf9628f395382d1ed5e8ad7777051430
UrbanXD <uadamvidi@gmail.com>
2024-12-28 16:44:08 +0100

    Bottomsheet updated

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,19 +5,16 @@ import {ScrollViewProvider} from "../features/core/context/ScrollViewProvider";
 import {useDatabase} from "../features/core/utils/database/Database";
 import { Session } from '@supabase/supabase-js';
 import {GestureHandlerRootView} from "react-native-gesture-handler";
-import {SafeAreaProvider, useSafeAreaInsets} from "react-native-safe-area-context";
+import {SafeAreaProvider} from "react-native-safe-area-context";
 import {BottomSheetModalProvider} from "@gorhom/bottom-sheet";
 import {store} from "../features/core/redux/store";
-import {theme} from "../features/core/constants/theme";
 import {Provider} from "react-redux";
 import {KeyboardProvider} from "react-native-keyboard-controller";
 import {StatusBar} from "expo-status-bar";
 import Header from "../features/core/components/header/Header";
-import {BottomSheetProvider} from "../features/core/context/BottomSheetProvider";
+import {BottomSheetProvider} from "../features/BottomSheet/context/BottomSheetProvider";
 import { DatabaseProvider } from '../features/core/context/DatabaseProvider';
-import Portal, {PortalHost, PortalProvider} from "@gorhom/portal";
-import {View} from "react-native";
-import {SEPARATOR_SIZES} from "../features/core/constants/constants";
+import { PortalProvider } from "@gorhom/portal";
 import Compactor from "../features/core/components/shared/Compactor";
 import {AlertProvider} from "../features/alert/context/AlertProvider";
 
@@ -27,7 +24,6 @@ const Layout:React.FC = () => {
 
     const database = useDatabase();
     const { supabaseConnector } = useDatabase();
-    const insets = useSafeAreaInsets();
 
     useEffect(() => {
         database.init();
@@ -57,8 +53,8 @@ const Layout:React.FC = () => {
             { Component: KeyboardProvider },
             { Component: GestureHandlerRootView, props: { style: {flex: 1} } },
             { Component: PortalProvider },
-            { Component: BottomSheetModalProvider }, // Ez előbb legyen
-            { Component: BottomSheetProvider }, // Ez utána
+            { Component: BottomSheetModalProvider },
+            { Component: BottomSheetProvider },
             { Component: ScrollViewProvider },
         ]}>
             <Stack screenOptions={{ header: () => <></>}} >
@@ -86,4 +82,5 @@ const RootLayout: React.FC = () => {
         </DatabaseProvider>
     )
 }
+
 export default RootLayout;
---
+++ src/features/BottomSheet/components/BottomSheetContainer.tsx
@@ -0,0 +1,52 @@
+import React, {ReactElement, ReactNode} from "react";
+import {StyleSheet, Text, View} from "react-native";
+import {BottomSheetView} from "@gorhom/bottom-sheet";
+import {FONT_SIZES, GLOBAL_STYLE, SEPARATOR_SIZES} from "../../core/constants/constants";
+import {useSafeAreaInsets} from "react-native-safe-area-context";
+
+interface BottomSheetContainerProps {
+    title: string
+    renderCloseButton?: () => ReactElement
+    children: ReactNode
+}
+
+const BottomSheetContainer: React.FC<BottomSheetContainerProps> = ({
+    title,
+    renderCloseButton,
+    children
+}) => {
+    const { top } = useSafeAreaInsets();
+    const styles = useStyles(top);
+
+    return (
+        <>
+            <View style={ styles.titleContainer }>
+                { renderCloseButton && renderCloseButton() }
+                <Text style={ styles.titleText }>
+                    { title }
+                </Text>
+            </View>
+            <BottomSheetView style={ styles.contentContainer }>
+                { children }
+            </BottomSheetView>
+        </>
+    );
+}
+
+const useStyles = (top: number) =>
+    StyleSheet.create({
+        titleContainer:{
+            paddingTop: top / 2
+        },
+        titleText: {
+            ...GLOBAL_STYLE.containerTitleText,
+            fontSize: FONT_SIZES.medium,
+            textAlign: "center"
+        },
+        contentContainer: {
+            flex: 1,
+            paddingHorizontal: SEPARATOR_SIZES.medium
+        }
+    })
+
+export default BottomSheetContainer;
