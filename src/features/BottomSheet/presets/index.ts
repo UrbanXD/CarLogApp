1a514c52f59927d97a46c15f4d4ebc190e25c154
UrbanXD <uadamvidi@gmail.com>
2025-02-15 22:49:10 +0100

    fix edit user bottomsheet export

--- src/features/BottomSheet/presets/index.ts
+++ src/features/BottomSheet/presets/index.ts
@@ -1,5 +1,6 @@
 export * from "./SignUpBottomSheet.tsx";
 export * from "./SignInBottomSheet.tsx";
 export * from "./SignUpVerificationBottomSheet.tsx";
+export * from "./EditUserBottomSheet.tsx";
 export * from "./ResetPasswordVerificationBottomSheet.tsx";
 export * from "./DeleteUserVerificationBottomSheet.tsx";
