ce169c886bd7b8f902da7e6dc5385ad89649a3fd
Urbán Ádám <h270385@stud.u-szeged.hu>
2025-02-19 09:38:35 +0100

    fix env types

---
+++ types/env.d.ts
@@ -0,0 +1,7 @@
+declare module '@env' {
+    export const SUPABASE_URL: string
+    export const SUPABASE_ANON_KEY: string
+    export const SUPABASE_BUCKET: string
+    export const RESEND_API_KEY: string
+    export const POWERSYNC_URL: string
+}
