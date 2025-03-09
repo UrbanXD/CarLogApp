cf7b6cc3af0aa06f32a017c44337484b8bbf3b3e
UrbanXD <uadamvidi@gmail.com>
2025-03-09 22:28:45 +0100

    added try catch to getImageFromAttachmentQueue

--- src/features/Database/utils/getImageFromAttachmentQueue.ts
+++ src/features/Database/utils/getImageFromAttachmentQueue.ts
@@ -3,10 +3,14 @@ import { encode } from "base64-arraybuffer";
 
 export const getImageFromAttachmentQueue = async (attachmentQueue?: PhotoAttachmentQueue, path?: string) => {
     if(attachmentQueue && path) {
-        const file = await attachmentQueue.getFile(path);
-        if(!file) return null;
+        try {
+            const file = await attachmentQueue.getFile(path);
+            if(!file) return null;
 
-        return { path, image: encode(file) }
+            return { path, image: encode(file) }
+        } catch (_) {
+            return null;
+        }
     }
     return null;
 }
