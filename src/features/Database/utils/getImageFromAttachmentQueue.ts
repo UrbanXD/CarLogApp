f4c923c862dd94f15005c97a529ece66b0460cb1
UrbanXD <uadamvidi@gmail.com>
2025-03-09 22:22:34 +0100

    added util func to get image from attachments

---
+++ src/features/Database/utils/getImageFromAttachmentQueue.ts
@@ -0,0 +1,12 @@
+import {PhotoAttachmentQueue} from "../connector/powersync/PhotoAttachmentQueue.ts";
+import {encode} from "base64-arraybuffer";
+
+export const getImageFromAttachmentQueue = async (attachmentQueue?: PhotoAttachmentQueue, path?: string) => {
+    if(attachmentQueue && path) {
+        const file = await attachmentQueue.getFile(path);
+        if(!file) return null;
+
+        return { path, image: encode(file) }
+    }
+    return null;
+}
