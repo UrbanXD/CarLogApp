8104921f96946ddd98c68abef130c707b123081f
UrbanXD <uadamvidi@gmail.com>
2025-08-14 19:23:30 +0200

    Remove search bar inside value state

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,32 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { ICON_NAMES } from "../constants/index.ts";
 import Input from "./Input/Input.ts";
 import { TextInputProps } from "./Input/text/TextInput.tsx";
 
-interface SearchBarProps {
-    setTerm: (value: string) => void;
-    term?: string;
-    textInputProps?: TextInputProps;
+type SearchBarProps = {
+    setTerm: (value: string) => void
+    term: string
+    textInputProps?: TextInputProps
 }
 
 const SearchBar: React.FC<SearchBarProps> = ({
     setTerm,
-    term = "",
+    term,
     textInputProps
 }) => {
-    const [value, setValue] = useState(term);
-
     const placeholder = textInputProps?.placeholder ?? "Keresés...";
-
-    useEffect(() => {
-        setTerm(value);
-    }, [value]);
-
 
     return (
         <Input.Text
-            value={ value }
-            setValue={ setValue }
+            value={ term }
+            setValue={ setTerm }
             { ...textInputProps }
             icon={ ICON_NAMES.search }
             placeholder={ placeholder }
