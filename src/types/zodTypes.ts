1630366c087f7388199322ebb7ecd64d16cd3d26
UrbanXD <uadamvidi@gmail.com>
2025-09-06 19:56:45 +0200

    Image type fix

--- src/types/zodTypes.ts
+++ src/types/zodTypes.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
-import { ImageType } from "../utils/pickImage.ts";
 import { ColorValue } from "react-native";
+import { Image } from "./index.ts";
 
 export const zNumber = (defaultValue?: number = NaN, minValue?: number = 0) => z
 .preprocess(
@@ -23,7 +23,7 @@ export const zPickerRequired = z
 .min(1, "Válasszon ki egy elemet!");
 
 export const zImage = z
-.custom<ImageType | null>(value => value === null || value instanceof ImageType);
+.custom<Image | null>(value => value === null || value instanceof Image);
 // .refine(
 //     files => [ 'image/jpg', 'image/jpeg', 'image/png' ].includes( files?.[ 0 ]?.type ),
 //     'Accepted Formats: JPG/JPEG/PNG'
