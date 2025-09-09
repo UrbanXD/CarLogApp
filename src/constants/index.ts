8622c3efd69d5407c975c77d5fded42bb4c02727
UrbanXD <uadamvidi@gmail.com>
2025-09-09 10:26:08 +0200

    Remove unused car make and model json

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -18,38 +18,3 @@ export interface ControllerRenderArgs {
 export const SIMPLE_HEADER_HEIGHT = hp(6.75);
 export const SIMPLE_TABBAR_HEIGHT = hp(7.5);
 export const ICON_FONT_SIZE_SCALE = 1.5;
-
-export interface CarBrandsType {
-    [key: string]: Array<CarModelsType>;
-}
-
-export interface CarModelsType {
-    name: string,
-    startYear: number,
-    endYear: number,
-}
-
-export const CARS_DATA = require("../assets/cars.json");
-
-export const GET_CARS = () => {
-    const cars_data = {} as CarBrandsType;
-
-    CARS_DATA.forEach((item: any) => {
-        const models = [] as Array<CarModelsType>;
-
-        item
-        .models
-        .map((item: any) => {
-            models.push({
-                name: item.name,
-                startYear: Number(item.years.startYear),
-                endYear: Number(item.years.endYear)
-            });
-        });
-
-        cars_data[item.brand] = models;
-    });
-    return cars_data;
-};
-
-export const CARS = GET_CARS();
