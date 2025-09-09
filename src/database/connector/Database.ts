c890fd629cb228a0f0fe7d7ce71134b33e47e76a
UrbanXD <uadamvidi@gmail.com>
2025-09-09 22:56:20 +0200

    Add car, model and make daos to database

--- src/database/connector/Database.ts
+++ src/database/connector/Database.ts
@@ -6,8 +6,10 @@ import { SupabaseStorageAdapter } from "./storage/SupabaseStorageAdapter.ts";
 import { PhotoAttachmentQueue } from "./powersync/PhotoAttachmentQueue.ts";
 import { AttachmentRecord } from "@powersync/attachments";
 import { UserDAO } from "../../features/user/model/dao/UserDAO.ts";
-import { CarDAO } from "../../features/car/model/dao/CarDAO.ts";
+import { CarDao } from "../../features/car/model/dao/CarDao.ts";
 import { BaseConfig } from "../../constants/index.ts";
+import { ModelDao } from "../../features/car/model/dao/ModelDao.ts";
+import { MakeDao } from "../../features/car/model/dao/MakeDao.ts";
 
 export class Database {
     powersync: AbstractPowerSyncDatabase;
@@ -16,7 +18,9 @@ export class Database {
     storage: SupabaseStorageAdapter;
     attachmentQueue?: PhotoAttachmentQueue;
     private _userDAO?: UserDAO;
-    private _carDAO?: CarDAO;
+    private _carDao?: CarDao;
+    private _makeDao?: MakeDao;
+    private _modelDao?: ModelDao;
 
     constructor() {
         this.powersync = new PowerSyncDatabase({
@@ -49,17 +53,27 @@ export class Database {
     }
 
     get userDAO(): UserDAO {
-        if(!this._userDAO) {
-            this._userDAO = new UserDAO(this.db);
-        }
+        if(!this._userDAO) this._userDAO = new UserDAO(this.db);
+
         return this._userDAO;
     }
 
-    get carDAO(): CarDAO {
-        if(!this._carDAO) {
-            this._carDAO = new CarDAO(this.db);
-        }
-        return this._carDAO;
+    get carDao(): CarDao {
+        if(!this._carDao) this._carDao = new CarDao(this.db, this.attachmentQueue);
+
+        return this._carDao;
+    }
+
+    get makeDao(): MakeDao {
+        if(!this._makeDao) this._makeDao = new MakeDao(this.db);
+
+        return this._makeDao;
+    }
+
+    get modelDao(): ModelDao {
+        if(!this._modelDao) this._modelDao = new ModelDao(this.db, this.makeDao);
+
+        return this._modelDao;
     }
 
     async init() {
