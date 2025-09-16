e5528d1b5ed55af9b6457974ff7a9d194f2df7ad
UrbanXD <uadamvidi@gmail.com>
2025-09-16 22:11:47 +0200

    Fix date picker style

--- src/components/Input/datePicker/DatePicker.tsx
+++ src/components/Input/datePicker/DatePicker.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import { DEFAULT_SEPARATOR, SEPARATOR_SIZES } from "../../../constants/index.ts";
-import { heightPercentageToDP } from "react-native-responsive-screen";
 import { CalendarPicker } from "./pickerView/CalendarPicker.tsx";
 import { WheelDatePicker } from "./pickerView/WheelDatePicker.tsx";
 import { useDatePicker } from "../../../contexts/datePicker/DatePickerContext.ts";
 import { DatePickerHeader } from "./header/DatePickerHeader.tsx";
 import { TimePicker } from "./pickerView/TimePicker.tsx";
 import Animated, { FadeIn, FadeOut } from "react-native-reanimated";
+import { heightPercentageToDP } from "react-native-responsive-screen";
 
 export function DatePicker() {
     const { currentView } = useDatePicker();
@@ -42,10 +42,12 @@ export function DatePicker() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        width: "100%",
         padding: DEFAULT_SEPARATOR,
         gap: SEPARATOR_SIZES.small
     },
     viewContainer: {
-        flex: 1, height: heightPercentageToDP(30), overflow: "hidden"
+        flex: 1,
+        height: heightPercentageToDP(30)
     }
 });
