2981ae6df9ab41a428455caf817b80b17aadb5bc
UrbanXD <uadamvidi@gmail.com>
2025-09-17 19:26:53 +0200

    Update Input row style based on new form theme

--- src/components/Input/common/InputRow.tsx
+++ src/components/Input/common/InputRow.tsx
@@ -1,14 +1,16 @@
 import React, { ReactElement } from "react";
 import { StyleSheet, View } from "react-native";
 import { SEPARATOR_SIZES } from "../../../constants/index.ts";
+import { formTheme } from "../../../ui/form/constants/theme.ts";
 
 type InputRowProps = {
-    children?: ReactElement;
+    children?: ReactElement
+    focused?: boolean
 }
 
-export function InputRow({ children }: InputRowProps) {
+export function InputRow({ children, focused = false }: InputRowProps) {
     return (
-        <View style={ styles.container }>
+        <View style={ [styles.container, focused && styles.focused] }>
             { children }
         </View>
     );
@@ -17,6 +19,15 @@ export function InputRow({ children }: InputRowProps) {
 const styles = StyleSheet.create({
     container: {
         flexDirection: "row",
-        gap: SEPARATOR_SIZES.small
+        gap: SEPARATOR_SIZES.small,
+        height: formTheme.containerHeight,
+        backgroundColor: formTheme.containerBackgroundColor,
+        borderRadius: formTheme.borderRadius,
+        paddingHorizontal: formTheme.containerPaddingHorizontal,
+        borderWidth: 1,
+        borderColor: formTheme.borderColor
+    },
+    focused: {
+        borderColor: formTheme.activeColor
     }
 });
