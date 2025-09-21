631db4033c60d167ed6b1a546a0dfffc5296b2f3
UrbanXD <uadamvidi@gmail.com>
2025-09-21 16:03:42 +0200

    Remove odometer log reducer

--- src/database/redux/index.ts
+++ src/database/redux/index.ts
@@ -2,13 +2,11 @@ import { combineReducers } from "@reduxjs/toolkit";
 import { carsReducer, CarsState } from "../../features/car/model/slice/index.ts";
 import { userReducer } from "../../features/user/model/slice/index.ts";
 import { alertReducer } from "../../ui/alert/model/slice/index.ts";
-import { carLogReducer } from "../../features/carLog/model/slice/index.ts";
 
 const rootReducer = combineReducers({
     cars: carsReducer,
     user: userReducer,
-    alert: alertReducer,
-    carLog: carLogReducer
+    alert: alertReducer
 });
 
 export default rootReducer;
