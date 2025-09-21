c3829f2242b74bae223cdae7328d0e9b31e1b842
UrbanXD <uadamvidi@gmail.com>
2025-09-21 17:59:00 +0200

    Add useCallback to getCar

--- src/features/car/hooks/useCars.ts
+++ src/features/car/hooks/useCars.ts
@@ -1,12 +1,13 @@
 import { useAppSelector } from "../../../hooks/index.ts";
 import { getCars, isLoading } from "../model/selectors/index.ts";
 import { Car } from "../schemas/carSchema.ts";
+import { useCallback } from "react";
 
 const useCars = () => {
     const cars = useAppSelector(getCars);
     const loading = useAppSelector(isLoading);
 
-    const getCar = (id: string): Car | undefined => cars.find(car => car.id === id);
+    const getCar = useCallback((id: string): Car | undefined => cars.find(car => car.id === id), [cars]);
 
     return { cars, loading, getCar };
 };
