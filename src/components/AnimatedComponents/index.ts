6e4b645aaeb1eca9aa5f2a554087d0ef05cec3a0
UrbanXD <uadamvidi@gmail.com>
2025-09-28 14:57:30 +0200

    Add animated icon

--- src/components/AnimatedComponents/index.ts
+++ src/components/AnimatedComponents/index.ts
@@ -3,9 +3,11 @@ import Svg, { Path } from "react-native-svg";
 import { Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { FlashList } from "@shopify/flash-list";
+import MaterialIcon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export const AnimatedSvg = Animated.createAnimatedComponent(Svg);
 export const AnimatedPath = Animated.createAnimatedComponent(Path);
 export const AnimatedPressable = Animated.createAnimatedComponent(Pressable);
 export const AnimatedSafeAreaView = Animated.createAnimatedComponent(SafeAreaView);
 export const AnimatedFlashList = Animated.createAnimatedComponent(FlashList);
+export const AnimatedMaterialIcon = Animated.createAnimatedComponent(MaterialIcon);
