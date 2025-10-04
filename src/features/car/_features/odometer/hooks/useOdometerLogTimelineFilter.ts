ad162854a31d09ea432410f906562281f6cd5b83
UrbanXD <uadamvidi@gmail.com>
2025-10-04 16:52:54 +0200

    Sort filter buttons in odometer log timeline

--- src/features/car/_features/odometer/hooks/useOdometerLogTimelineFilter.ts
+++ src/features/car/_features/odometer/hooks/useOdometerLogTimelineFilter.ts
@@ -1,35 +1,41 @@
 import { FilterButtonProps } from "../../../../../components/filter/FilterButton.tsx";
-import { useMemo, useState } from "react";
-import { COLORS } from "../../../../../constants/index.ts";
+import { useEffect, useState } from "react";
 import { OdometerLogTypeEnum } from "../model/enums/odometerLogTypeEnum.ts";
+import { useDatabase } from "../../../../../contexts/database/DatabaseContext.ts";
+import { OdometerLogType } from "../schemas/odometerLogTypeSchema.ts";
+import { ExpenseType } from "../../../../expense/schemas/expenseTypeSchema.ts";
 
 export function useOdometerLogTimelineFilter() {
-    const [typeFilter, setTypeFilter] = useState<OdometerLogTypeEnum | null>(null);
+    const { odometerLogTypeDao } = useDatabase();
+    const [types, setTypes] = useState<Array<ExpenseType>>([]);
+    const [typeFilter, setTypeFilter] = useState<OdometerLogType["id"] | null>(null);
 
-    const filterButtons: Array<FilterButtonProps> = useMemo(() => [
-        {
-            title: "Mind",
-            active: !typeFilter,
-            onPress: () => setTypeFilter(null)
-        },
-        {
-            title: "Egyéb",
-            active: typeFilter === OdometerLogTypeEnum.SIMPLE,
-            onPress: () => setTypeFilter(OdometerLogTypeEnum.SIMPLE)
-        },
-        {
-            title: "Tankolás",
-            active: typeFilter === OdometerLogTypeEnum.FUEL,
-            activeColor: COLORS.fuelYellow,
-            onPress: () => setTypeFilter(OdometerLogTypeEnum.FUEL)
-        },
-        {
-            title: "Szervíz",
-            active: typeFilter === OdometerLogTypeEnum.SERVICE,
-            activeColor: COLORS.service,
-            onPress: () => setTypeFilter(OdometerLogTypeEnum.SERVICE)
-        }
-    ], [typeFilter]);
+    useEffect(() => {
+        (async () => {
+            const types = await odometerLogTypeDao.getAll();
+
+            // sort based on locale
+            const sorted = types.sort((a, b) => {
+                //Make simple first
+                if(a.id === OdometerLogTypeEnum.SIMPLE) return -1;
+                if(b.id === OdometerLogTypeEnum.SIMPLE) return 1;
+
+                return a.locale.localeCompare(b.locale);
+            });
+
+
+            setTypes(sorted);
+        })();
+    }, []);
+
+    const filterButtons: Array<FilterButtonProps> = types.map((type) => ({
+        title: type.locale,
+        active: typeFilter === type.id,
+        activeColor: type.primaryColor ?? undefined,
+        onPress: () => setTypeFilter(type.id)
+    }));
+
+    filterButtons.unshift({ title: "Mind", active: !typeFilter, onPress: () => setTypeFilter(null) });
 
     return { typeFilter, filterButtons };
 }
