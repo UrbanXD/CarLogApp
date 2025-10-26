a3ea7c61368c30089d32dc13c3e7812289a2f05a
UrbanXD <uadamvidi@gmail.com>
2025-10-27 00:14:29 +0100

    Add remove button  to service item view

--- src/features/expense/_features/service/components/ServiceItemView.tsx
+++ src/features/expense/_features/service/components/ServiceItemView.tsx
@@ -1,17 +1,31 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { ServiceItem } from "../schemas/serviceItemSchema.ts";
 import React from "react";
-import { COLORS, FONT_SIZES, SEPARATOR_SIZES } from "../../../../../constants/index.ts";
+import { COLORS, FONT_SIZES, ICON_NAMES, SEPARATOR_SIZES } from "../../../../../constants/index.ts";
+import Button from "../../../../../components/Button/Button.ts";
 
 type ServiceItemProps = {
     item: ServiceItem
     onPress?: () => void
+    onDelete?: () => void
 }
 
-export function ServiceItemView({ item, onPress }: ServiceItemProps) {
+export function ServiceItemView({ item, onPress, onDelete }: ServiceItemProps) {
     return (
         <Pressable onPress={ onPress } disabled={ !onPress } style={ styles.container }>
             <View style={ styles.labelContainer }>
+                {
+                    onDelete &&
+                   <Button.Icon
+                      icon={ ICON_NAMES.close }
+                      iconSize={ FONT_SIZES.p3 }
+                      iconColor={ COLORS.gray2 }
+                      width={ FONT_SIZES.p3 }
+                      height={ FONT_SIZES.p3 }
+                      backgroundColor="transparent"
+                      onPress={ onDelete }
+                   />
+                }
                 <Text style={ styles.itemText }>
                     { item.type.key }
                     <Text style={ styles.countText }> (x{ item.quantity })</Text>
@@ -46,7 +60,9 @@ const styles = StyleSheet.create({
         paddingVertical: SEPARATOR_SIZES.lightSmall / 2
     },
     labelContainer: {
-        flex: 1
+        flex: 1,
+        flexDirection: "row",
+        alignItems: "center"
     },
     itemText: {
         fontFamily: "Gilroy-Medium",
