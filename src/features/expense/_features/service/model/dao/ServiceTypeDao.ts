0b531b2c7b3092cbe05b97ba11655e5e77b591b8
UrbanXD <uadamvidi@gmail.com>
2025-11-09 12:42:54 +0100

    Fix paginator options

--- src/features/expense/_features/service/model/dao/ServiceTypeDao.ts
+++ src/features/expense/_features/service/model/dao/ServiceTypeDao.ts
@@ -39,7 +39,7 @@ export class ServiceTypeDao extends Dao<ServiceTypeTableRow, ServiceType, Servic
         return new CursorPaginator<ServiceTypeTableRow, PickerItemType>(
             this.db,
             SERVICE_TYPE_TABLE,
-            { field: ["key", "id"], order: "asc" },
+            { cursor: [{ field: "key", order: "asc" }, { field: "id" }], defaultOrder: "asc" },
             {
                 perPage,
                 mapper: this.mapper.entityToPickerItem.bind(this.mapper)
