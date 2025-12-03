aafa6d63cf8733389dad02ffefbf3976ff76aa45
UrbanXD <uadamvidi@gmail.com>
2025-12-03 23:31:07 +0100

    Update layout of fuel stat and translations HU

---
+++ src/features/statistics/components/stats/FuelStatistics.tsx
@@ -0,0 +1,237 @@
+import { useDatabase } from "../../../../contexts/database/DatabaseContext.ts";
+import React, { useCallback, useEffect, useState } from "react";
+import { SummaryStat, TrendStat } from "../../model/dao/statisticsDao.ts";
+import { LineChartView } from "../charts/LineChartView.tsx";
+import { StyleSheet, View } from "react-native";
+import { SEPARATOR_SIZES } from "../../../../constants/index.ts";
+import { Currency } from "../../../_shared/currency/schemas/currencySchema.ts";
+import { useTranslation } from "react-i18next";
+import dayjs from "dayjs";
+import { MasonryStatView } from "../MasonryStatView.tsx";
+import { formatWithUnit } from "../../../../utils/formatWithUnit.ts";
+import { StatCard } from "../StatCard.tsx";
+import { ChartTitle } from "../charts/common/ChartTitle.tsx";
+
+type FuelConsumptionStatisticsProps = {
+    carId?: string
+    currency?: Currency
+    from: string
+    to: string
+}
+
+export function FuelStatistics({
+    carId,
+    currency,
+    from,
+    to
+}: FuelConsumptionStatisticsProps) {
+    const { t } = useTranslation();
+    const { statisticsDao } = useDatabase();
+
+    const [fuelConsumption, setFuelConsumption] = useState<TrendStat | null>(null);
+    const [fuelLogSummary, setFuelLogSummary] = useState<{ quantity: SummaryStat, amount: SummaryStat } | null>(null);
+
+    useEffect(() => {
+        (async () => {
+            const resultFuelLogStat = await statisticsDao.getFuelSummary({
+                carId: carId,
+                from,
+                to
+            });
+
+            setFuelLogSummary(resultFuelLogStat);
+            setFuelConsumption(await statisticsDao.getFuelConsumption({ carId, from, to }));
+        })();
+    }, [carId, from, to]);
+
+    const getFuelCount = useCallback(() => {
+        return {
+            label: t("statistics.fuel.log_count"),
+            value: fuelLogSummary ? `${ fuelLogSummary.quantity.count } ${ t("common.count") }` : null,
+            isPositive: undefined,
+            trend: null,
+            trendDescription: null,
+            isLoading: !fuelLogSummary
+        };
+    }, [fuelLogSummary, t]);
+
+    const getFuelTotalQuantity = useCallback(() => {
+        console.log(fuelLogSummary?.quantity.max);
+        return {
+            label: t("statistics.fuel.total_quantity"),
+            value: fuelLogSummary
+                   ? formatWithUnit(fuelLogSummary.quantity.total, fuelLogSummary.quantity?.unitText)
+                   : null,
+            isPositive: fuelLogSummary?.quantity.totalTrend.isTrendPositive,
+            trend: fuelLogSummary && fuelLogSummary?.quantity
+                   ? `${ fuelLogSummary.quantity.totalTrend.trendSymbol } ${ formatWithUnit(
+                    fuelLogSummary.quantity.totalTrend.trend,
+                    fuelLogSummary.quantity?.unitText
+                ) }`
+                   : null,
+            trendDescription: fuelLogSummary ? t("statistics.compared_to_previous_cycle") : null,
+            isLoading: !fuelLogSummary
+        };
+    }, [fuelLogSummary, t]);
+
+    const getFuelAverageQuantity = useCallback(() => {
+        return {
+            label: t("statistics.fuel.avg_quantity"),
+            value: fuelLogSummary
+                   ? formatWithUnit(fuelLogSummary.quantity.average, fuelLogSummary.quantity?.unitText)
+                   : null,
+            isPositive: fuelLogSummary?.quantity.averageTrend.isTrendPositive,
+            trend: fuelLogSummary
+                   ? `${ fuelLogSummary.quantity.averageTrend.trendSymbol } ${ formatWithUnit(
+                    fuelLogSummary.quantity.averageTrend.trend,
+                    fuelLogSummary.quantity?.unitText
+                ) }`
+                   : null,
+            trendDescription: fuelLogSummary ? t("statistics.compared_to_previous_cycle") : null,
+            isLoading: !fuelLogSummary
+        };
+    }, [fuelLogSummary, t]);
+
+    const getFuelMedianQuantity = useCallback(() => {
+        return {
+            label: t("statistics.fuel.median_quantity"),
+            value: fuelLogSummary
+                   ? formatWithUnit(fuelLogSummary.quantity.median, fuelLogSummary.quantity?.unitText)
+                   : null,
+            isPositive: fuelLogSummary?.quantity.medianTrend.isTrendPositive,
+            trend: fuelLogSummary
+                   ? `${ fuelLogSummary.quantity.medianTrend.trendSymbol } ${ formatWithUnit(
+                    fuelLogSummary.quantity.medianTrend.trend,
+                    fuelLogSummary.quantity?.unitText
+                ) }`
+                   : null,
+            trendDescription: fuelLogSummary ? t("statistics.compared_to_previous_cycle") : null,
+            isLoading: !fuelLogSummary
+        };
+    }, [fuelLogSummary, t]);
+
+    const getFuelMaxQuantity = useCallback(() => {
+        return {
+            label: t("statistics.fuel.max_quantity"),
+            value: fuelLogSummary?.quantity?.max ? formatWithUnit(
+                fuelLogSummary.quantity.max.value,
+                fuelLogSummary.quantity?.unitText
+            ) : null,
+            isLoading: !fuelLogSummary
+        };
+    }, [fuelLogSummary, t]);
+
+    const getFuelTotalAmount = useCallback(() => {
+        return {
+            label: t("statistics.fuel.total_amount"),
+            value: fuelLogSummary ? formatWithUnit(fuelLogSummary.amount.total, fuelLogSummary.amount?.unitText) : null,
+            isPositive: fuelLogSummary?.amount.totalTrend.isTrendPositive,
+            trend: fuelLogSummary
+                   ? `${ fuelLogSummary.amount.totalTrend.trendSymbol } ${ formatWithUnit(
+                    fuelLogSummary.amount.totalTrend.trend,
+                    fuelLogSummary.amount?.unitText
+                ) }`
+                   : null,
+            trendDescription: fuelLogSummary ? t("statistics.compared_to_previous_cycle") : null,
+            isLoading: !fuelLogSummary
+        };
+    }, [fuelLogSummary, currency, t]);
+
+    const getFuelAverageAmount = useCallback(() => {
+        return {
+            label: t("statistics.fuel.avg_amount"),
+            value: fuelLogSummary
+                   ? formatWithUnit(fuelLogSummary.amount.average, fuelLogSummary.amount?.unitText)
+                   : null,
+            isPositive: fuelLogSummary?.amount.averageTrend.isTrendPositive,
+            trend: fuelLogSummary
+                   ? `${ fuelLogSummary.amount.averageTrend.trendSymbol } ${ formatWithUnit(
+                    fuelLogSummary.amount.averageTrend.trend,
+                    fuelLogSummary.amount?.unitText
+                ) }`
+                   : null,
+            trendDescription: fuelLogSummary ? t("statistics.compared_to_previous_cycle") : null,
+            isLoading: !fuelLogSummary
+        };
+    }, [fuelLogSummary, currency, t]);
+
+    const getFuelMedianAmount = useCallback(() => {
+        return {
+            label: t("statistics.fuel.median_amount"),
+            value: fuelLogSummary
+                   ? formatWithUnit(fuelLogSummary.amount.median, fuelLogSummary.amount?.unitText)
+                   : null,
+            isPositive: fuelLogSummary?.amount.medianTrend.isTrendPositive,
+            trend: fuelLogSummary
+                   ? `${ fuelLogSummary.amount.medianTrend.trendSymbol } ${ formatWithUnit(
+                    fuelLogSummary.amount.medianTrend.trend,
+                    fuelLogSummary.amount?.unitText
+                ) }`
+                   : null,
+            trendDescription: fuelLogSummary ? t("statistics.compared_to_previous_cycle") : null,
+            isLoading: !fuelLogSummary
+        };
+    }, [fuelLogSummary, currency, t]);
+
+    const getFuelMaxAmount = useCallback(() => {
+        return {
+            label: t("statistics.fuel.max_amount"),
+            value: fuelLogSummary?.amount?.max ? formatWithUnit(
+                fuelLogSummary.amount.max.value,
+                fuelLogSummary.amount?.unitText
+            ) : null,
+            isLoading: !fuelLogSummary
+        };
+    }, [fuelLogSummary, currency, t]);
+
+    return (
+        <View style={ styles.container }>
+            <View style={ styles.statCardContainer }>
+                <StatCard { ...getFuelCount() } />
+                <ChartTitle title={ "Kiadsok" }/>
+                <MasonryStatView
+                    column1={ [
+                        getFuelTotalAmount(),
+                        getFuelMaxAmount()
+                    ] }
+                    column2={ [
+                        getFuelAverageAmount(),
+                        getFuelMedianAmount()
+                    ] }
+                />
+                <ChartTitle title={ "Kiadsok" }/>
+                <MasonryStatView
+                    column1={ [
+                        getFuelTotalQuantity(),
+                        getFuelMaxQuantity()
+                    ] }
+                    column2={ [
+                        getFuelAverageQuantity(),
+                        getFuelMedianQuantity()
+                    ] }
+                />
+            </View>
+            <LineChartView
+                title={ {
+                    title: t("statistics.fuel.fuel_consumption"),
+                    unit: fuelConsumption?.unitText,
+                    description: fuelConsumption && fuelConsumption.lineChartData.length > 0 && t(
+                        "statistics.fuel.fuel_consumption_accuracy")
+                } }
+                chartData={ fuelConsumption?.lineChartData }
+                formatValue={ (value) => formatWithUnit(value, fuelConsumption?.unitText) }
+                formatLabel={ (label) => dayjs(label).format("L") }
+                isLoading={ !fuelConsumption }
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create(({
+    container: {
+        gap: SEPARATOR_SIZES.small
+    },
+    statCardContainer: {
+        gap: SEPARATOR_SIZES.small
+    }
+}));
