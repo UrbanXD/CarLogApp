eceb3f5ac3b4647e5f657684925d3634f0c69e71
UrbanXD <uadamvidi@gmail.com>
2025-12-12 09:10:21 +0100

    Fix db date type format

--- src/features/statistics/utils/formatDateToDatabaseFormat.ts
+++ src/features/statistics/utils/formatDateToDatabaseFormat.ts
@@ -2,5 +2,5 @@ import { DateType } from "react-native-ui-datepicker";
 import dayjs from "dayjs";
 
 export function formatDateToDatabaseFormat(date: DateType) {
-    return dayjs(date).utc().format("YYYY-MM-DD HH:mm:ss.SSS") + "+00";
+    return dayjs(date).utc().format("YYYY-MM-DD HH:mm:ss.SSS") + "Z";
 }
