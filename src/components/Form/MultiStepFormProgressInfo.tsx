03b382614a7bc622013fd160d66b2a21c6408764
UrbanXD <uadamvidi@gmail.com>
2025-12-15 02:16:51 +0100

    Remove skia and replace progress info with svg + reanimated

--- src/components/Form/MultiStepFormProgressInfo.tsx
+++ src/components/Form/MultiStepFormProgressInfo.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect } from "react";
 import { useMultiStepForm } from "../../contexts/multiStepForm/MultiStepFormContext.ts";
 import { useSharedValue, withTiming } from "react-native-reanimated";
-import { useFont } from "@shopify/react-native-skia";
 import { heightPercentageToDP as hp } from "react-native-responsive-screen";
-import ProgressInfo from "../ProgressInfo.tsx";
+import { ProgressInfo } from "../ProgressInfo.tsx";
 import { useTranslation } from "react-i18next";
 
 const MultiStepFormProgressInfo: React.FC = () => {
@@ -26,9 +25,6 @@ const MultiStepFormProgressInfo: React.FC = () => {
         end.value = withTiming(Number(currentStepText) / realStepsCount, { duration: 1500 });
     }, [currentStep]);
 
-    const font = useFont(require("../../assets/fonts/Gilroy-Heavy.otf"), hp(3));
-    if(!font) return <></>;
-
     const showsProgressInfo = ((!isFirstStep && !isLastStep) || (isFirstStep && isFirstCount) || (isLastStep && isLastCount));
 
     return (
@@ -39,7 +35,6 @@ const MultiStepFormProgressInfo: React.FC = () => {
                   radius={ hp(6) }
                   strokeWidth={ hp(1.25) }
                   end={ end }
-                  font={ font }
                   statusText={ `${ realStepsCount } / ${ currentStepText }` }
                   stepTitle={ t(steps[currentStep]?.title ?? "common.unknown") }
                   stepSubtitle={
