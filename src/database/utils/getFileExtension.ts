2071e7b251e346d12d5d6c56221e56eba4dc9f1d
UrbanXD <uadamvidi@gmail.com>
2025-12-24 16:38:47 +0100

    Update media type getter util

--- src/database/utils/getFileExtension.ts
+++ src/database/utils/getFileExtension.ts
@@ -1,13 +1,33 @@
-export const getFileExtension = (path?: string) => {
+export function getFileExtension(path?: string) {
     if(!path) return "";
 
     const fileName = path.split("/").pop() || path;
     return fileName.substring(fileName.lastIndexOf(".") + 1) || "jpg";
-};
+}
 
-export const getFileName = (path?: string) => {
+export function getFileName(path?: string) {
     if(!path) return "";
 
     const fileName = path.split("/").pop() || path;
     return fileName.substring(0, fileName.lastIndexOf(".")) || fileName;
+}
+
+const mimeTypes: Record<string, string> = {
+    "jpg": "image/jpeg",
+    "jpeg": "image/jpeg",
+    "png": "image/png",
+    "gif": "image/gif",
+    "svg": "image/svg+xml",
+    "webp": "image/webp",
+    "mp4": "video/mp4",
+    "webm": "video/webm",
+    "pdf": "application/pdf",
+    "json": "application/json"
 };
+
+export function getMediaType(path?: string) {
+    if(!path) return null;
+
+    const extension = getFileExtension(path).toLowerCase();
+    return mimeTypes?.[extension] ?? "application/octet-stream";
+}
