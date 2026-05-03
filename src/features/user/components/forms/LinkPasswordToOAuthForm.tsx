733ded7e19ecff7273408299147e1bdf95abfe50
UrbanXD <uadamvidi@gmail.com>
2026-05-03 20:32:59 +0200

    Fix missing email at add password to google oauth

--- src/features/user/components/forms/LinkPasswordToOAuthForm.tsx
+++ src/features/user/components/forms/LinkPasswordToOAuthForm.tsx
@@ -1,26 +1,27 @@
 import { useAlert } from "../../../../ui/alert/hooks/useAlert.ts";
 import { FormState, useForm } from "react-hook-form";
-import { PasswordStep } from "./steps/index.ts";
+import { PasswordStep } from "./steps";
 import { getToastMessage } from "../../../../ui/alert/utils/getToastMessage.ts";
-import { AddPasswordToast } from "../../presets/toast/index.ts";
+import { AddPasswordToast } from "../../presets/toast";
 import { NewPasswordRequest, useNewPasswordFormProps } from "../../schemas/form/newPasswordRequest.ts";
 import { useAuth } from "../../../../contexts/auth/AuthContext.ts";
 import Form from "../../../../components/Form/Form.tsx";
 import React from "react";
 import { useDatabase } from "../../../../contexts/database/DatabaseContext.ts";
-import { SubmitHandlerArgs } from "../../../../types/index.ts";
-import { InvalidFormToast } from "../../../../ui/alert/presets/toast/index.ts";
+import { SubmitHandlerArgs } from "../../../../types";
+import { InvalidFormToast } from "../../../../ui/alert/presets/toast";
 
 type LinkPasswordToOAuthFormProps = {
+    defaultEmail?: string
     onFormStateChange?: (formState: FormState<NewPasswordRequest>) => void
 }
 
-export function LinkPasswordToOAuthForm({ onFormStateChange }: LinkPasswordToOAuthFormProps) {
+export function LinkPasswordToOAuthForm({ defaultEmail, onFormStateChange }: LinkPasswordToOAuthFormProps) {
     const { supabaseConnector } = useDatabase();
     const { openToast } = useAlert();
     const { hasPassword, refreshSession } = useAuth();
 
-    const form = useForm<NewPasswordRequest>(useNewPasswordFormProps());
+    const form = useForm<NewPasswordRequest>(useNewPasswordFormProps(defaultEmail));
 
     const submitHandler: SubmitHandlerArgs<NewPasswordRequest> = {
         onValid: async (request) => {
